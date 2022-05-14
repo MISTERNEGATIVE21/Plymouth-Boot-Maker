da622595c706051fd388c9d43dbbbb9bfb4797b9
MISTER_NEGATIVE <supergogetavegito21@gmail.com>
2022-05-14 23:26:12 +0530

    Update s.sh

--- s.sh
+++ s.sh
@@ -3,11 +3,11 @@
 echo Please enter your sudo password if you are prompted to do so!
 
 echo Installing dependencies using apt...
-sudo apt install python2 python-gi python-tk ffmpeg
-echo Done!
+sudo apt install python3 && sudo apt install python3-gi python3-gi-cairo gir1.2-gtk-3.0 && python3-tk && ffmpeg
+echo Done "If ffmpeg not installed use sudo snapd install ffmpeg"
 
 echo Running program...
-python2 plymouth-creator.py
+python3 plymouth-creator.py
 echo Done!
 
 echo Have a nice day!
